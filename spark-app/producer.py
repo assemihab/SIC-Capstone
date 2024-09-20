3aa1f45c1cf29454e5c5829a8af6392641fd1056
Yahya Zakaria <78583513+yahia997@users.noreply.github.com>
2024-09-20 08:02:20 +0300

    Update producer.py

--- spark-app/producer.py
+++ spark-app/producer.py
@@ -17,16 +17,24 @@ producer = KafkaProducer(
   value_serializer = lambda x:dumps(x).encode('utf-8')  # to make as json format
 )
 
-date_format = "%Y-%m-%d"
+date_format = "%Y-%m-%d %H:%M:%S"
 
 # add streaming messeges to the kafka topic
 for row in rows[1:]: # to skip header row
+
+  # combine date and hour to match our format
+  # Ex. 11.0 => 11:00
+  # Ex. 5.0 => 05:00
+  my_time = row[2]
+  hour = int(row[3][:-2])
+  my_time += (" " if hour > 9 else " 0") + str(hour) + ":00:00"
+  print(my_time)
   
   # to make data able to be parsed as json
   log = {
     "id": row[1],
     "temperature": float(row[4] or 0),
-    "date": str(datetime.strptime(row[2], date_format)),
+    "date": str(datetime.strptime(my_time, date_format)),
     "hour": float(row[3]),
     "calories": float(row[6] or 0),
     "distance": float(row[7] or 0),
